f5e568c4f1cfbfbd1ab8dde55f0ada6a2fb5d33f
Yumin Chen <yumin@chen.so>
2022-07-05 14:51:54 +0100

    Initial commit

---
+++ src/gatsby-node.js
@@ -0,0 +1,44 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const { createPath } = require('gatsby-page-utils');
+
+const openapiVersion = '3.0.3';
+
+const createSpec = (source, definition) =>
+  swaggerJSDoc({
+    definition: {
+      openapi: openapiVersion,
+      ...definition,
+    },
+    apis: source,
+  });
+
+exports.createPages = ({ actions }, pluginOptions) => {
+  const {
+    uiRoute = '/api',
+    source = ['src/api/**/*.js'],
+    definition = {},
+  } = pluginOptions;
+
+  actions.createPage({
+    path: createPath(uiRoute),
+    component: require.resolve(`${__dirname}/components/swagger.js`),
+    context: { apiSpec: createSpec(source, definition) },
+  });
+};
+
+exports.pluginOptionsSchema = ({ Joi }) => {
+  return Joi.object({
+    uiRoute: Joi.string()
+      .default('/api')
+      .description('Route to the Swagger UI.'),
+    source: Joi.array()
+      .items(Joi.string())
+      .default(['src/api/**/*.js'])
+      .description(
+        'Paths of the source files to scan for `@openapi` annotations in JSDoc-style comments.'
+      ),
+    definition: Joi.object()
+      .default({})
+      .description('Optional. Any definition to add to the spec.'),
+  });
+};
